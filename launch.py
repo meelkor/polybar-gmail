97893159bc09b88ac839c6049b9471a813daf7a9
Jan Seda <seda@spaceknow.com>
2019-04-27 00:43:15 +0200

    Add optional subject of newest email

--- launch.py
+++ launch.py
@@ -13,6 +13,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--prefix', default='\uf0e0')
 parser.add_argument('-c', '--color', default='#e06c75')
 parser.add_argument('-ns', '--nosound', action='store_true')
+parser.add_argument('-t', '--title', action='store_true',
+    help='When provided, subject of newest email is also displayed')
+parser.add_argument('--title-format', default=': %s',
+    help='When title is enabled, this option specifies its format')
 args = parser.parse_args()
 
 DIR = os.path.dirname(os.path.realpath(__file__))
@@ -22,25 +26,47 @@ unread_prefix = '%{F' + args.color + '}' + args.prefix + ' %{F-}'
 error_prefix = '%{F' + args.color + '}\uf06a %{F-}'
 count_was = 0
 
-def print_count(count, is_odd=False):
+def get_subject(message):
+    headers = message['payload']['headers']
+    return next(h for h in headers if h["name"] == "Subject")["value"]
+
+def format_count(count, is_odd=False):
     tilde = '~' if is_odd else ''
     output = ''
     if count > 0:
         output = unread_prefix + tilde + str(count)
     else:
         output = (args.prefix + ' ' + tilde).strip()
-    print(output, flush=True)
+    return output
 
 def update_count(count_was):
     gmail = discovery.build('gmail', 'v1', credentials=file.Storage(CREDENTIALS_PATH).get())
-    labels = gmail.users().labels().get(userId='me', id='INBOX').execute()
-    count = labels['messagesUnread']
-    print_count(count)
+
+    messagesQuery = gmail.users().messages().list(userId='me', q="is:unread").execute()
+    count = messagesQuery["resultSizeEstimate"]
+
+    out = format_count(count)
+
+    if count > 0 and args.title:
+        messages = messagesQuery["messages"]
+        message = gmail.users().messages().get(
+                userId='me',
+                id=messages[0]['id'],
+                format='metadata',
+                metadataHeaders=['Subject']).execute()
+        out += args.title_format % get_subject(message)
+
     if not args.nosound and count_was < count and count > 0:
         subprocess.run(['canberra-gtk-play', '-i', 'message'])
+
+    print(out, flush=True)
     return count
 
-print_count(0, True)
+def print_prev():
+    print(format_count(count_was), flush=True)
+
+
+print_prev()
 
 while True:
     try:
@@ -51,7 +77,7 @@ while True:
             print(error_prefix + 'credentials not found', flush=True)
             time.sleep(2)
     except (errors.HttpError, ServerNotFoundError, OSError) as error:
-        print_count(count_was, True)
+        print_prev()
         time.sleep(5)
     except client.AccessTokenRefreshError:
         print(error_prefix + 'revoked/expired credentials', flush=True)
